1e79f1eb0ba114ebaf43025316ffe64672f01375
User Asagal <asagal@finn.neuropunks.org>
2011-08-20 17:14:25 -0400

    JSON request flow, dummy JSON.

---
+++ lib-web/js/local/config.js
@@ -0,0 +1,11 @@
+/*
+ * Establishes VD as global namespace and instantiates config.
+ */
+VD = {};
+VD.config =
+{
+    apiUrl : "/api/",
+    defaultStart : 0,
+    queryInterval : 7, //in seconds
+    slideshowTimeout : 5 //in seconds
+}
