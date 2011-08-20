47fe4e7b170bda7965fd39bbcdae777f8cd351e7
User Asagal <asagal@finn.neuropunks.org>
2011-08-20 17:40:07 -0400

    Point at Max's data feed.

--- lib-web/js/local/config.js
+++ lib-web/js/local/config.js
@@ -5,7 +5,8 @@ VD = {};
 VD.config =
 {
     mapContainerName : 'map-container',
-    apiUrl : "/api/",
+    //apiUrl : "/api/",
+    apiUrl : "/~max/voronoi/backend-kohana/venues.json",
     defaultStart : 0,
     queryInterval : 7, //in seconds
     slideshowTimeout : 5 //in seconds
