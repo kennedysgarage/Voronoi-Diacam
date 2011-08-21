e68b9248941b51c175209ea42428d4c9bfc425ff
Max Gribov <max@neuropunks.org>
2011-08-21 14:42:42 -0400

    fix up face updating and dont pull really large images

--- backend-kohana/application/classes/controller/faces.php
+++ backend-kohana/application/classes/controller/faces.php
@@ -5,54 +5,34 @@ class Controller_Faces extends Controller {
 	public function action_index()
 	{
         require "/home/max/public_html/voronoi/phpFlickr-3.1/helpers.php";
+
+        $key = $_REQUEST['key'];
+        $secret = $_REQUEST['secret'];
+
         $faces = $urls = array();
         $model = new Model_faces();
 
     
         // first 30
-        $data1 = $model->Update();
-        foreach ($data1 as $d) {
-            if ($d['url'] == 'http://farm7.static.flickr.com/6182/6064414202_873c205fab.jpg') continue;
-
-            $urls[$d['hash']] = $d['url'];
-            $hashes[$d['url']] = $d['hash'];
-        }
-        $isf = is_face($urls, $hashes);
-
-        if (count($isf['yes'])) {
-            foreach ($isf['yes'] as $face_data) {
-                $model->setFace($face_data);
-                $faces[] = $face_data;
+        for ($i = 0; $i = 1; $i++) {
+            $data1 = $model->Update();
+            foreach ($data1 as $d) {
+                $urls[$d['hash']] = $d['url'];
+                $hashes[$d['url']] = $d['hash'];
             }
-        }
+            $isf = is_face($urls, $hashes, $key, $secret);
 
-        if (count($isf['no'])) {
-            foreach ($isf['no'] as $face_data) {
-                $model->setFace($face_data, false);
+            if (count($isf['yes'])) {
+                foreach ($isf['yes'] as $face_data) {
+                    $model->setFace($face_data);
+                    $faces[] = $face_data;
+                }
             }
-        }
 
-
-        // next 30
-        $urls = $ifs = $hashes  = array();
-        $data2 = $model->Update();
-        foreach ($data2 as $d) {
-            if ($d['url'] == 'http://farm7.static.flickr.com/6182/6064414202_873c205fab.jpg') continue;
-            $urls[$d['hash']] = $d['url'];
-            $hashes[$d['url']] = $d['hash'];
-        }
-        $isf = is_face($urls, $hashes);
-
-        if (count($isf['yes'])) {
-            foreach ($isf['yes'] as $face_data) {
-                $model->setFace($face_data);
-                $faces[] = $face_data;
-            }
-        }
-
-        if (count($isf['no'])) {
-            foreach ($isf['no'] as $face_data) {
-                $model->setFace($face_data, false);
+            if (count($isf['no'])) {
+                foreach ($isf['no'] as $face_data) {
+                    $model->setFace($face_data, false);
+                }
             }
         }
 
