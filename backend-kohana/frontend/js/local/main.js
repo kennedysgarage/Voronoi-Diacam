164a8deda128eef93bc36337c6a6f64bb45a33d7
User Asagal <asagal@finn.neuropunks.org>
2011-08-21 06:21:04 -0400

    Working, with refresh.
    - NEEDS BETTER DATA!!
    - Currently completely re-renders map
    - Miniature size, otherwise voronoi looks like crap

--- backend-kohana/frontend/js/local/main.js
+++ backend-kohana/frontend/js/local/main.js
@@ -12,15 +12,14 @@ VD.main = function() {
 var index = 0;
 
 
-var w = 1260,
-    h = 703;
+var w = 200,
+    h = 90;
 
 /*
 var vertices = d3.range(20).map(function(d) {
   return [Math.random() * w, Math.random() * h];
 });
 */
-//window.vertices = vertices;
 //var vertices = [[100,20],[200,200],[130,180],[350,400]];
 /*
 var images = [
@@ -47,6 +46,12 @@ var images = [
 ]
 */
 
+var svg = d3.select("#"+config.mapContainerName)
+  .append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
+//    .attr("class", "PiYG")
+
 function redraw( data ) {
 
         console.debug( 'redrawing...', $(config.mapContainerName) );
@@ -55,10 +60,12 @@ function redraw( data ) {
     // calculate min max range multiplier
     var minlat = null, minlong = null, maxlat = null, maxlong = null;
     for( var idx in data ) {
+      if( data.hasOwnProperty( idx )) {
         if( !minlat || data[idx].lat - minlat < 0 ) minlat = data[idx].lat;
         if( !maxlat || data[idx].lat - maxlat > 0 ) maxlat = data[idx].lat;
         if( !minlong || data[idx].long - minlong < 0 ) minlong = data[idx].long;
         if( !maxlong || data[idx].long - maxlong > 0 ) maxlong = data[idx].long;
+      }
     }
 
     var latrange = maxlat - minlat;
@@ -66,21 +73,21 @@ function redraw( data ) {
     var latmult = w/latrange;
     var longmult = h/longrange;
 
-console.debug('lmlm:',minlat,maxlat,minlong,maxlong,latrange,longrange,latmult,longmult);
+//console.debug('lmlm:',minlat,maxlat,minlong,maxlong,latrange,longrange,latmult,longmult);
 
     var vertices = [];
-    var images = [];
     for( var idx in data ) {
-        vertices.push( [Math.round((latmult*(data[idx].lat-minlat))),Math.round((longmult*(data[idx].long-minlong)))] );
-        console.debug(vertices[idx],data[idx].url);
-        images.push( data[idx].url );
+      if( data.hasOwnProperty( idx )) {
+        //TODO: REMOVE RANDOM!!!
+        vertices.push( [Math.round(Math.random()*10+(latmult*(data[idx].lat-minlat))),Math.round((latmult*(data[idx].long-minlong)))] ); //latmult preserves aspect ratio
+        //console.debug(vertices[idx],data[idx].url);
+      }
     }
 
-var svg = d3.select("#"+config.mapContainerName)
-  .append("svg:svg")
-    .attr("width", w)
-    .attr("height", h);
-//    .attr("class", "PiYG")
+window.vertices = vertices;
+
+svg.selectAll("path")
+    .remove();
 
 svg.selectAll("path")
     .data(d3.geom.voronoi(vertices))
@@ -88,7 +95,7 @@ svg.selectAll("path")
 //    .attr("class", function(d, i) { return i ? "q" + (i % 9) + "-9" : null; })
     .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
     .on("mousemove", update)
-    .attr("stroke", "yellow") 
+    //.attr("stroke", "yellow") 
     .attr("fill", function(d,i) {
     
         var defs = d3.select("#defs")
@@ -98,17 +105,17 @@ svg.selectAll("path")
             .attr("width", "100%")
             .attr("height", "100%")
             .append("svg:image")
-            .attr("width", "600")
-            .attr("height", "450")
-            .attr("x", vertices[i][0]-300)
-            .attr("y", vertices[i][1]-225)
-            .attr("xlink:href", images[i]);
+            .attr("width", data[i].sizex)
+            .attr("height", data[i].sizey)
+            .attr("x", vertices[i][0]-data[i].sizex/2)
+            .attr("y", vertices[i][1]-data[i].sizey/2)
+            .attr("xlink:href", data[i].url);
 
-//console.debug(
-
+window.svg = svg;
 
         return "url(#img" + (i+1) +")" 
     });
+
 /*
 svg.selectAll("circle")
     .data(vertices.slice(1))
@@ -121,9 +128,9 @@ svg.selectAll("circle")
 
 
 function update( arg1, arg2 ) {
-  console.debug( arg1, arg2, vertices[0] );
+  //console.debug( arg1, arg2, vertices[0] );
 
-  vertices[0] = d3.svg.mouse(this);
+  //vertices[0] = d3.svg.mouse(this);
 
 /*
 svg.selectAll("path")
@@ -142,7 +149,7 @@ window.pollServer = pollServer;
     /* Procedural flow: */
     // - Set polling on an interval
     pollServer();
-    //setInterval( pollServer, config.queryInterval * 1000 );
+    setInterval( pollServer, config.queryInterval * 1000 );
 
     function pollServer() {
         var request = new Request.JSON({
