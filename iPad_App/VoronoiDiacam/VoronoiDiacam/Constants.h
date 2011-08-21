886002c1d6b76367fa15459e7911febbba814d36
David Cilia <dev@oprisks.com>
2011-08-21 08:01:08 -0400

    added 4square oauth

---
+++ iPad_App/VoronoiDiacam/VoronoiDiacam/Constants.h
@@ -0,0 +1,21 @@
+//
+//  Constants.h
+//  VoronoiDiacam
+//
+//  Created by David Cilia on 8/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define CLIENT_ID CZ4WIS44DKGQIONQSLOM1N4KF05USYAP4JUPWBACSRDMLYEE
+#define CLIENT_SECRET UGKTR2VJCPTEIUKFRCXZX4EOJ24H4FMTWAOPE452ULNMJKJF
+
+#define Setting com.oprisksolutions.beta.${PRODUCT_NAME:rfc1034identifier}
+
+
+@interface Constants : NSObject {
+    
+}
+
+@end
---
+++ iPad_App/VoronoiDiacam/VoronoiDiacam/Constants.m
@@ -0,0 +1,14 @@
+//
+//  Constants.m
+//  VoronoiDiacam
+//
+//  Created by David Cilia on 8/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Constants.h"
+
+
+@implementation Constants
+
+@end
