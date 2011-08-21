d1f45a11af125795da108d7a797fe8df604b7d2f
Max Gribov <max@neuropunks.org>
2011-08-21 14:43:31 -0400

    cron

---
+++ crontab.sh
@@ -0,0 +1,7 @@
+*/1     *   *   *   *   cd /home/max/public_html/voronoi/phpFlickr-3.1 && (/usr/bin/php /home/max/public_html/voronoi/phpFlickr-3.1/flickr2json.php | /usr/local/bin/psql -U pgsql maadd) > /dev/null 2>&1
+*/1     *   *   *   *   /usr/local/bin/curl -m 160 "http://max.neuropunks.org/faces?key=c910346114b46fec0868280a52a0f9be&secret=92d7f3632b5d1bc16f046461702368cd"  > /dev/null 2>&1
+*/1     *   *   *   *   /usr/local/bin/curl -m 160 "http://max.neuropunks.org/faces?key=aa687bece400c220c3f51e3956c6ce8c&secret=81ed2ae812ea71dccd7f8e560db10b47"  > /dev/null 2>&1
+*/1     *   *   *   *   /usr/local/bin/curl -m 160 "http://max.neuropunks.org/faces?key=60caa943020fcdc1b5b8d3fabfb34006&secret=98ac3b7ea6a3632016931d9a37327234"  > /dev/null 2>&1
+*/1     *   *   *   *   /usr/local/bin/curl -m 160 "http://max.neuropunks.org/faces?key=f1daa075801e030d7e69149c82744126&secret=8c811091f2f8e2a640bdfac66dd08ac7"  > /dev/null 2>&1
+*/1     *   *   *   *   /usr/local/bin/curl -m 160 "http://max.neuropunks.org/faces?key=c072ee565a3a868cffb3935674ea05dc&secret=621ae566c832778a4d7336216da4406b"  > /dev/null 2>&1
+
